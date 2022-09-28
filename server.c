d9cd79b1711b7f459ffdb2a6b1c5135802c744da
ngr0223 <ngr0223@gmail.com>
2022-09-28 11:15:38 +0800

    Initial commit

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+int InitServer(char *pServerAddr, u_int16_t server_port);
+
+int InitServer(char *pServerAddr, u_int16_t server_port)
+{
+
+}
